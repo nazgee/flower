76205b0094c715370d94672c7d168a59dc9a0634
Michal Stawinski <michal.stawinski@gmail.com>
2012-04-24 22:12:21 +0200

    Created Consts class

---
+++ src/eu/nazgee/game/flower/Consts.java
@@ -0,0 +1,7 @@
+package eu.nazgee.game.flower;
+
+public @interface Consts {
+	public static final String MENU_FONT = "ELEKTRA.ttf";
+	public static final int CAMERA_WIDTH = 480;
+	public static final int CAMERA_HEIGHT = 800;
+}
