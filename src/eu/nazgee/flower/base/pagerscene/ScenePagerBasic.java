9d36be44b1185c7706c6d1c8666ae655e82ef26b
Michal Stawinski <michal.stawinski@sonymobile.com>
2012-06-21 18:39:23 +0200

    Added new class for pager scenes

---
+++ src/eu/nazgee/flower/base/pagerscene/ScenePagerBasic.java
@@ -0,0 +1,70 @@
+package eu.nazgee.flower.base.pagerscene;
+
+import org.andengine.entity.IEntity;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+import eu.nazgee.util.LayoutBase;
+
+abstract public class ScenePagerBasic<T extends IEntity> extends ScenePager<T> {
+	// ===========================================================
+	// Constants
+	// ===========================================================
+
+	// ===========================================================
+	// Fields
+	// ===========================================================
+	private final int ROWS;
+	private final int COLS;
+	private final float EFFECTIVE_PAGE_WIDTH_RATIO;
+	private final float PAGE_FLIP_RATIO;
+	// ===========================================================
+	// Constructors
+	// ===========================================================
+	public ScenePagerBasic(float W, float H,
+			VertexBufferObjectManager pVertexBufferObjectManager,
+			int pColums, int pRows, float pEffectivePageWidthRatio, float pPageFlipRatio) {
+		super(W, H, pVertexBufferObjectManager, (int) (W * pEffectivePageWidthRatio * pPageFlipRatio));
+
+		COLS = pColums;
+		ROWS = pRows;
+		EFFECTIVE_PAGE_WIDTH_RATIO = pEffectivePageWidthRatio;
+		PAGE_FLIP_RATIO = pPageFlipRatio;
+	}
+	// ===========================================================
+	// Getter & Setter
+	// ===========================================================
+	public int getRrows() {
+		return ROWS;
+	}
+
+	public int getCols() {
+		return COLS;
+	}
+
+	public float getEffectiveWidth() {
+		return getW() * EFFECTIVE_PAGE_WIDTH_RATIO;
+	}
+
+	public float getPageFlip() {
+		return getEffectiveWidth() * PAGE_FLIP_RATIO;
+	}
+	// ===========================================================
+	// Methods for/from SuperClass/Interfaces
+	// ===========================================================
+	@Override
+	protected void attachPage(IPage pPage, int pPageNumber) {
+		pPage.setPosition(pPageNumber * getW() * getEffectiveWidth(), 0);
+		attachChild(pPage);
+		LayoutBase.setPosBottomLeft(pPage, pPageNumber * getW() * getEffectiveWidth(), 0);
+	}
+	// ===========================================================
+	// Methods
+	// ===========================================================
+
+
+
+	// ===========================================================
+	// Inner and Anonymous Classes
+	// ===========================================================
+
+}
